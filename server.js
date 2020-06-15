b42501d34b69621edddc7e999bd17b021676d1af
Jessie-JX-Cai <65533649+Jessie-JX-Cai@users.noreply.github.com>
2020-06-15 22:45:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.get('/api-endpoint', function(request, response) {
     response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     response.setHeader('Access-Control-Allow-Credentials', true);
-
     response.send(JSON.parse(JSON.stringify(jsonContent)));
     
 });
